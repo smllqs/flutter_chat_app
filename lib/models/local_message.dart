591ad75dce9cfe654105cc0121af690541eab8de
somelelequse <QSXSOM001@myuct.ac.za>
2023-12-11 11:07:38 +0200

    chat service added

---
+++ lib/models/local_message.dart
@@ -0,0 +1,30 @@
+import 'package:chat/chat.dart';
+
+class LocalMessage {
+  String chatId;
+  Message message;
+  ReceiptStatus receiptStatus;
+
+  String get id => _id;
+  late String _id;
+
+  LocalMessage(this.chatId, this.message, this.receiptStatus);
+
+  Map<String, dynamic> toMap() => {
+        'chat_id': chatId,
+        'id': message.id,
+        ...message.toJson(),
+        'receipt': receiptStatus.value()
+      };
+
+  factory LocalMessage.fromMap(Map<String, dynamic> json) {
+    final message = Message(
+        from: json['from'],
+        to: json['to'],
+        timestamp: json['timestamp'],
+        contents: json['contents']);
+    final localMessage =
+        LocalMessage(json['chat_id'], message, json['receipt_status']);
+    return localMessage;
+  }
+}
