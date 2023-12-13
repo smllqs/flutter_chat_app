0265154cd098191d1817836ece640617a0a137c0
somelelequse <QSXSOM001@myuct.ac.za>
2023-12-13 03:32:43 +0200

    out of commit messages

--- test/data/sqflite_data_source_test.dart
+++ test/data/sqflite_data_source_test.dart
@@ -1,27 +1,23 @@
 import 'package:chat/chat.dart';
 import 'package:flutter_chat_app/data/data_source/sqflite_data_source.dart';
 import 'package:flutter_chat_app/models/chat.dart';
+import 'package:flutter_chat_app/models/local_message.dart';
 import 'package:flutter_test/flutter_test.dart';
 // import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 // import 'package:sqflite/sqflite.dart';
-
-// class MockSqfliteDatabase extends Mock implements Database {}
-
-// class MockBatch extends Mock implements Batch {}
 
 import 'package:mockito/annotations.dart';
 import 'package:sqflite/sqflite.dart';
 
 import 'sqflite_data_source_test.mocks.dart';
 
-@GenerateMocks([Database, Batch])
+@GenerateMocks([Database])
 void main() {
   late SQfliteDataSource sut;
   late MockDatabase database;
   // MockBatch batch;
   setUp(() {
-    print('object');
     database = MockDatabase();
     // batch = MockBatch();
     sut = SQfliteDataSource(database);
@@ -37,7 +33,6 @@ void main() {
 
   test('should perform insert of chat to the database', () async {
     final chat = Chat('1234');
-    // print(database.isOpen);
     when(database.insert('chats', chat.toMap(),
             conflictAlgorithm: ConflictAlgorithm.replace))
         .thenAnswer((_) async => 1);
@@ -45,6 +40,19 @@ void main() {
     await sut.addChat(chat);
 
     verify(database.insert('chats', chat.toMap(),
+            conflictAlgorithm: ConflictAlgorithm.replace))
+        .called(1);
+  });
+
+  test('should perform insert of message to the database', () async {
+    final localMessage = LocalMessage('1234', message, ReceiptStatus.sent);
+
+    when(database.insert('messages', localMessage.toMap(),
+            nullColumnHack: null, conflictAlgorithm: ConflictAlgorithm.replace))
+        .thenAnswer((_) async => 1);
+    await sut.addMessage(localMessage);
+
+    verify(database.insert('messages', localMessage.toMap(),
             conflictAlgorithm: ConflictAlgorithm.replace))
         .called(1);
   });
